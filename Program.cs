1c625a5291a46fb1a9c6053eb4ed5fce65d9aba7
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2024-01-22 18:50:09 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Prog_224_L2_012224
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // Containment
+            // Aggregation
+
+            // Inheritence
+            // How To
+            // Polymorphism
+
+            // virtual
+            // override
+            // new
+
+
+        } // Main
+
+    } // class
+
+} // namespace
